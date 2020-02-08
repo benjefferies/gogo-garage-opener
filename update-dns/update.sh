75b0a0017ac9a52e51730e4139afdc4e95bb45d5
Ben Jefferies <benjefferies@echosoft.uk>
2020-02-08 09:42:50 +0000

    Remove git clone and add working dir

--- update-dns/update.sh
+++ update-dns/update.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env sh
 set -e
 
-apt-get update && apt-get install curl -y
-git clone https://github.com/benjefferies/gogo-garage-opener.git
-pushd gogo-garage-opener/update-dns
 terraform init
 terraform apply -var ip_address=$(curl ifconfig.co) -var domain=mygaragedoor.space -var a_record_domain=open.mygaragedoor.space -auto-approve
-popd
-rm -r gogo-garage-opener
