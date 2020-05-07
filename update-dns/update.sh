e31c223f1858fc0cf2d87c9832076f3f9ada0e45
Ben Jefferies <benjefferies@echosoft.uk>
2020-05-07 13:26:09 +0100

    Use terraform natively

--- update-dns/update.sh
+++ update-dns/update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
-terraform init
-terraform apply -var ip_address=$(curl ifconfig.co) -var domain=mygaragedoor.space -var a_record_domain=open.mygaragedoor.space -auto-approve
+curl -L -o /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_arm.zip
+unzip -o /tmp/terraform.zip -d /tmp
+chmod +x /tmp/terraform
+/tmp/terraform init
+/tmp/terraform apply -var ip_address=$(curl ifconfig.co) -var domain=mygaragedoor.space -var a_record_domain=open.mygaragedoor.space -auto-approve
