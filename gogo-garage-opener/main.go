ca24e920463538e3076fcb7f2a005cc93b82b062
Ben Jefferies <benjefferies@echosoft.uk>
2020-05-07 13:33:24 +0100

    Add ping for monitoring

--- gogo-garage-opener/main.go
+++ gogo-garage-opener/main.go
@@ -73,6 +73,7 @@ func registerResources(userDao UserDao, pinDao PinDao, garageDoorDao GarageDoorD
 	router := mux.NewRouter()
 	userResource.register(router)
 	garageDoorResource.register(router)
+	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) { w.WriteHeader(200) }).Name("ping")
 	return router
 }
 
